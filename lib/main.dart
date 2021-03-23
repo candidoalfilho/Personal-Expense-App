52dd0bbc24f9e2a1170ee79920e5c864690915b5
Candido Alfredo <candidoalfilho@hotmail.com>
2021-03-23 18:47:25 -0300

    A better styling

--- lib/main.dart
+++ lib/main.dart
@@ -47,11 +47,30 @@ class MyHomePage extends StatelessWidget {
                 return Card(
                     child: Row(
                   children: [
-                    Container(child: Text(tx.amount.toString())),
-                    Column(children: [
-                      Text(tx.title),
-                      Text(tx.date.toString())
-                    ])
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        child: Text(tx.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ))),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tx.title,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Text(tx.date.toString(),
+                          style: TextStyle(color: Colors.grey))
+                        ])
                   ],
                 ));
               }).toList(),
