e797340fbb7ebad5e7d191be09e6ba62f7b17651
Candido Alfredo <candidoalfilho@hotmail.com>
2021-03-24 10:58:31 -0300

    Fetching user input

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyHomePage extends StatelessWidget {
         date: DateTime.now())
   ];
 
+  //String titleInput;
+  // String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,14 +57,32 @@ class MyHomePage extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    TextField(decoration: InputDecoration(labelText: 'Title'),),
-                    TextField(decoration: InputDecoration(labelText: 'Amount'),),
-                    TextButton(onPressed: () {  },
-                    child: Text('Add Transaction',),
-                    style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.purple)),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: titleController,
+                      //onChanged: (val) {
+                      // titleInput = val;
+                      //},
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: amountController,
+                      //  onChanged: (val) {
+                      //  amountInput = val;
+                      //},
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print(titleController.text);
+                      },
+                      child: Text(
+                        'Add Transaction',
+                      ),
+                      style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.purple)),
                     ),
                   ],
-                  
                 ),
               ),
             ),
