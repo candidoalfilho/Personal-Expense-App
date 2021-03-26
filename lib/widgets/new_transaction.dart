c76f2fe2a0b5c72be9eac732cc54a2879f3bc049
Candido Alfredo <candidoalfilho@hotmail.com>
2021-03-25 21:33:33 -0300

    Improving & Styling the Modal bottom sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
 void submitData(){
   final enteredTitle =titleController.text;
@@ -13,8 +20,11 @@ void submitData(){
   if(enteredTitle.isEmpty || enteredAmount <=0){
     return;
   }
-  addTx(enteredTitle, enteredAmount);
+  widget.addTx(enteredTitle, enteredAmount);
+
+  Navigator.of(context).pop();
 }
+
   @override
   Widget build(BuildContext context) {
     return Card(
